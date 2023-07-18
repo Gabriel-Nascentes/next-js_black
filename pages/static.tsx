1ac0c05c42535c47d017510987e42dd5cfa17b9c
Gabriel Nascentes <gno.gabriel@gmail.com>
2023-07-17 21:00:42 -0300

    Criando Api para serem utilizadas no Next.js

--- pages/static.tsx
+++ pages/static.tsx
@@ -1,4 +1,4 @@
-import { GetServerSideProps, NextPage } from "next";
+import { GetStaticProps, NextPage } from "next";
 import { Container, Row, Col } from "react-bootstrap";
 import { useState, useEffect, ReactNode } from "react";
 
@@ -7,20 +7,21 @@ interface ApiResponse {
   timestamp: Date;
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
-  const serverSideData: ApiResponse = await fetch(
+export const getStaticProps: GetStaticProps = async () => {
+  const staticData = await fetch(
     `${process.env.NEXT_PUBLIC_APIURL}/api/hello`
   ).then((response) => response.json());
   return {
     props: {
-      serverSideData,
+      staticData,
     },
+    revalidate: 10,
   };
 };
 
-const Dynamic: NextPage = (props: {
+const Static: NextPage = (props: {
   children?: ReactNode;
-  serverSideData?: ApiResponse;
+  staticData?: ApiResponse;
 }) => {
   const [clientSideData, setClientSideData] = useState<ApiResponse>();
 
@@ -36,8 +37,8 @@ const Dynamic: NextPage = (props: {
       <h1 className="my-5">Como funcionam as renderizações do Next.js</h1>
       <Row>
         <Col>
-          <h3>Gerado no servidor:</h3>
-          <h2>{props.serverSideData?.timestamp.toString()}</h2>
+          <h3>Gerado no estaticamente durante o build:</h3>
+          <h2>{props.staticData?.timestamp.toString()}</h2>
         </Col>
         <Col>
           <h3>Gerado no cliente:</h3>
@@ -48,4 +49,4 @@ const Dynamic: NextPage = (props: {
   );
 };
 
-export default Dynamic;
+export default Static;
